3229435a3cb5600b9184880cd47eb01d9118a2de
kmou424 <me@kmou424.moe>
2023-02-18 23:57:20 +0800

    refactor: move user-agent presets definitions to external py

--- login.py
+++ login.py
@@ -1,9 +1,11 @@
 import argparse
-import requests
 import uuid
 
+import requests
 from netifaces import interfaces, ifaddresses, AF_INET
+
 from config import ACCOUNT, PASSWORD
+from useragent import USER_AGENT_PRESETS
 
 
 def get_mac_addr() -> str:
@@ -21,18 +23,6 @@ def get_ip_by_interface(interface: str) -> list:
             ret.append(i['addr'])
     return ret
 
-
-USER_AGENT_PRESETS = {
-    "GooglePixel6": "Mozilla/5.0 (Linux; Android 12; Pixel 6 Build/SD1A.210817.023; wv) AppleWebKit/537.36 (KHTML, "
-                    "like Gecko) Version/4.0 Chrome/94.0.4606.71 Mobile Safari/537.36",
-    "iPhone13ProMax": "Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, "
-                      "like Gecko) Version/10.0 Mobile/19A346 Safari/602.1",
-    "Windows10Edge": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                     "Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
-    "MacOSXSafari": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) "
-                    "Version/9.0.2 Safari/601.3.9",
-    "LinuxFirefox": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
-}
 
 body = "loginType=&auth_type=0&isBindMac1=1&pageid=1&templatetype=1&listbindmac=1&recordmac=0&isRemind=0&loginTimes" \
        "=&groupId=&distoken=&echostr=&url=http://edge-http.microsoft.com&isautoauth=&notice_pic_loop1=/portal" \
---
+++ useragent.py
@@ -0,0 +1,11 @@
+USER_AGENT_PRESETS = {
+    "GooglePixel6": "Mozilla/5.0 (Linux; Android 12; Pixel 6 Build/SD1A.210817.023; wv) AppleWebKit/537.36 (KHTML, "
+                    "like Gecko) Version/4.0 Chrome/94.0.4606.71 Mobile Safari/537.36",
+    "iPhone13ProMax": "Mozilla/5.0 (iPhone14,3; U; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, "
+                      "like Gecko) Version/10.0 Mobile/19A346 Safari/602.1",
+    "Windows10Edge": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                     "Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+    "MacOSXSafari": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) "
+                    "Version/9.0.2 Safari/601.3.9",
+    "LinuxFirefox": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+}
